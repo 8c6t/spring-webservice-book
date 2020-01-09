f0b511134b748a4dd35958d56c55a3abdde1eaf4
8c6t <nkw8620@gmail.com>
2020-01-09 10:29:12 +0900

    ch01. 인텔리제이로 스프링 부트 시작하기
    spring initializr 이용.

---
+++ src/test/java/com/hachicore/springwebservicebook/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hachicore.springwebservicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
